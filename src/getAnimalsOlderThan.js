e8d98d95a2ae330ea7c1eca30c538829cb3048df
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-25 18:20:03 -0300

    Requisito 1 e 2 OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = data.species.filter(({ name }) => animal === name);
+  // const ageVerification = animals.residents.every((element) => element.age < age);
+  const ageVerification = animals.every(({ residents }) => residents.every((info) => info.age > age));
+  return ageVerification;
 }
+
+
+//console.log(getAnimalsOlderThan('otters', 7));
+//console.log(data.species[0].residents[0].age);
+
+
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,5 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => ids.map((input) =>
-  data.species.filter(({ id }) => id === input));
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-  '78460a91-f4da-4dea-a469-86fd2b8ccc84'));
+const getSpeciesByIds = (...ids) => data.species.filter(({ id }) => ids.includes(id));
 
 module.exports = getSpeciesByIds;
