9a39ca3490d1b0c3818add97837ccba8f61f66ef
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-25 18:25:16 -0300

    Refatora o requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,16 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+const getAnimalsOlderThan = (animal, age) => {
   const animals = data.species.filter(({ name }) => animal === name);
-  // const ageVerification = animals.residents.every((element) => element.age < age);
-  const ageVerification = animals.every(({ residents }) => residents.every((info) => info.age > age));
+  const ageVerification = animals.every(({ residents }) =>
+    residents.every((info) => info.age > age));
   return ageVerification;
-}
-
-
-//console.log(getAnimalsOlderThan('otters', 7));
-//console.log(data.species[0].residents[0].age);
-
-
+};
 
 module.exports = getAnimalsOlderThan;
