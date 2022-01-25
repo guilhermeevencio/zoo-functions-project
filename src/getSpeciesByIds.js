a3b3f9daaf3f22fc9bf6e715c8379fe6a4c564fc
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-25 17:41:20 -0300

    Requisito 1 parcialmente ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => ids.map((input) =>
+  data.species.filter(({ id }) => id === input));
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  '78460a91-f4da-4dea-a469-86fd2b8ccc84'));
 
 module.exports = getSpeciesByIds;
