f83f3037f8254d8916d761bfac79bbdaaf46d5a7
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-26 09:16:49 -0300

    Requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,14 +2,16 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   const obj = {};
-  let count = 0;
   if (!animal) {
     data.species.forEach(({ name, residents }) => { obj[name] = residents.length; });
     return obj;
   }
   if (Object.values(animal).length === 2) {
-    // tenho que retornar o valor de quantos animais do mesmo sexo possuem... provavelmente irei com o filter e depois irei atualizar o count com o length.
+    const selectedAnimal = data.species.find(({ name }) => animal.specie === name)
+      .residents.filter(({ sex }) => sex === animal.sex);
+    return selectedAnimal.length;
   }
+  return data.species.find(({ name }) => animal.specie === name).residents.length;
 }
-console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
+console.log(countAnimals({ specie: 'bears' }));
 module.exports = countAnimals;
