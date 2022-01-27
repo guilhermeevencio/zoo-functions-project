6494647300e1254e6d8b8ea5fed7193e3a45e1bc
Guilherme <guilhermeevencioluz@gmail.com>
2022-01-27 12:28:12 -0300

    Requisito 8 OK

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,40 +1,43 @@
 const data = require('../data/zoo_data');
 
 const { hours } = data;
-// console.log(Object.keys(hours));
+const weekDays = Object.keys(hours);
+const animals = data.species.map(({ name }) => name);
+const comparison = (scheduleTarget) =>
+  (weekDays.includes(scheduleTarget) || animals.includes(scheduleTarget));
+
 function comparingDays(day) {
   const animalList = [];
   data.species.forEach(({ availability, name }) => {
-    if (availability.includes(day)) {
-      animalList.push(name);
-    }
+    if (availability.includes(day)) animalList.push(name);
   });
   return animalList;
 }
-
-function animalSchedule() {
-  const weekDays = Object.keys(hours);
-  const mapping = weekDays.map((day) => comparingDays(day));
-  return mapping;
-}
-
+const animalExhibition = (scheduleTarget) =>
+  data.species.find(({ name }) => scheduleTarget === name).availability;
+const animalSchedule = () => weekDays.map((day) => comparingDays(day));
+const monday = () => ({ officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' });
+const scheduleDays = (hour, index) =>
+  ({
+    officeHour: `Open from ${hour.open}am until ${hour.close}pm`,
+    exhibition: animalSchedule()[index],
+  });
+const schedule = Object.values(hours).reduce((acc, hour, index) => {
+  acc[weekDays[index]] = weekDays[index] !== 'Monday'
+    ? scheduleDays(hour, index) : monday();
+  return acc;
+}, {});
+const onlyOneDaySchedule = (scheduleTarget) => {
+  const find = Object.entries(schedule).find((arr) => arr.includes(scheduleTarget));
+  const obj = {};
+  const [a, b] = find;
+  obj[a] = b;
+  return obj;
+};
 function getSchedule(scheduleTarget) {
-  const weekDays = Object.keys(hours);
-  if (scheduleTarget === 'Monday') {
-    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
-  }
-  const schedule = Object.values(hours).reduce((acc, hour, index) => {
-    if (!scheduleTarget) {
-      const schedObj = {
-        officeHour: `Open from ${hour.open}am until ${hour.close}pm`,
-        exhibition: animalSchedule()[index],
-      };
-      acc[weekDays[index]] = weekDays[index] !== 'Monday'
-        ? schedObj : { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
-    }
-    return acc;
-  }, {});
-  return schedule;
+  if (scheduleTarget === 'Monday') return ({ Monday: monday() });
+  if (!scheduleTarget || (!comparison(scheduleTarget))) return schedule;
+  if (weekDays.includes(scheduleTarget)) return onlyOneDaySchedule(scheduleTarget);
+  return animalExhibition(scheduleTarget);
 }
-console.log(getSchedule());
 module.exports = getSchedule;
